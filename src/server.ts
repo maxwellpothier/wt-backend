4fb070408f9532be8b5d82551bd2e8a3fc3768fb
maxwellpothier <pothiermaxwell@gmail.com>
2022-12-04 15:16:39 -0700

    testing some errors

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-	console.log("Hello from express");
-	res.status(200);
-	res.json({message: "Message from the api", secondMessage: req.middlewareMessage});
+	throw new Error("Hello");
+	// console.log("Hello from express");
+	// res.status(200);
+	// res.json({message: "Message from the api", secondMessage: req.middlewareMessage});
 });
 
 app.use("/api", protectDataCalls, router);
 app.use("/identity", identityRouter);
+
+// Error handler
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.json({message: "Something went wrong"});
+});
 
 export default app;
