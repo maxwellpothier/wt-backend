4a469a0242611fa685332c15c9872fc2e7ff8d2b
maxwellpothier <pothiermaxwell@gmail.com>
2022-12-03 21:37:20 -0700

    adds basic error validation

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,15 +1,31 @@
 import {Router} from "express";
 import { createPost, deletePost, editPost, getAllPosts, getUserPosts } from "../handlers/post";
+import {body} from "express-validator";
+import { handleInputErrors } from "../utils/middlewareUtils";
 
 const router = Router();
+
+const inputValidators = {
+	post: [
+		body("content").isString(),
+		handleInputErrors
+	],
+	album: [
+		body("title").isString(),
+		body("artist").isString(),
+		body("yearReleased").isString(),
+		body("url").isString(),
+		handleInputErrors
+	],
+};
 
 /**
  * POSTS
  */
 router.get("/posts", getAllPosts);
 router.get("/posts/:userid", getUserPosts);
-router.post("/posts/:userid", createPost);
-router.put("/posts/:userid/:id", editPost);
+router.post("/posts/:userid", inputValidators.post, createPost);
+router.put("/posts/:userid/:id", inputValidators.post, editPost);
 router.delete("/posts/:userid/:id", deletePost);
 
 /**
@@ -17,6 +33,6 @@ router.delete("/posts/:userid/:id", deletePost);
  */
 router.get("/albums", () => {});
 router.get("/albums/:id", () => {});
-router.post("/albums", () => {});
+router.post("/albums", inputValidators.album, () => {});
 
 export default router;
