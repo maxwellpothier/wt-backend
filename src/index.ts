007b5f5fb6398be11d175d7b8961fff212132ea0
maxwellpothier <pothiermaxwell@gmail.com>
2022-12-06 20:55:34 -0700

    removing console logs

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ dotenv.config();
 import app from "./server";
 import config from "./config";
 
-const port = 4550;
-
-console.log("Here's the config", config);
-
 app.listen(config.port, () => {
 	console.log(`Server listening on port ${config.port}`)
 })
